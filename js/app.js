ade8ae3cbf581ef0a8f9b12c3e0bb94968450c99
Jakob Vesely <jakob@appartig.at>
2021-09-18 18:55:48 +0200

    Ready Version

--- js/app.js
+++ js/app.js
@@ -1,74 +1,119 @@
 $(document).ready(() => {
 
-    $('.color-theme-toggle').click(() => {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches)
+    {
         $('body').toggleClass(['color-theme--light', 'color-theme--dark']);
-    });
+    }
 
-    console.debug("ready");
+    $('.color-theme-toggle').click(() => $('body').toggleClass(['color-theme--light', 'color-theme--dark']));
+
+    _ = new Typed('#greeting-typing', {
+        stringsElement: '#greeting',
+        typeSpeed: 30,
+        onStringTyped: () => setTimeout(() => $('.typed-cursor').remove(), 2000)
+    });
 
     getProjects();
 });
 
-function init ()
-{
-    console.log("init");
-    anime({
-        targets: '.item',
-        translateX: "500px",
-        loop: true,
-        duration: 3000
-    });
+const toggleImpressum = () => {
+
+    if ($('.impressum').hasClass('hidden')){
+        $('.impressum').show();
+        $('.impressum').css({height: 'auto'});
+        let h = $('.impressum').height();
+        $('.impressum').css({height: '0px'});
+        $('.impressum').animate({height: h}, 200, () => $('.impressum').toggleClass(['hidden', 'shown']));
+    }else{
+        $('.impressum').toggleClass(['hidden', 'shown']);
+        setTimeout(() => $('.impressum').animate({height: 0}, 200), 300);
+    }
 }
 
-function getProjects (category, startIndex, total, inWork = false)
+const getProjects = () =>
 {
     $.get("/projects.json", (projects) =>
     {
-        if (inWork == true && category == '*')
-        {
-            
-        }
-        console.debug(projects);
-
-        projects.forEach(project => {
-            let item = getBuiltItem(project);
-            $("#" + project.category + " > .right").append(item);
+        projects.forEach(item => {
+            let project = new Project(item);
+            let element = project.getElement();
+            $("#" + project.category + " > .right > .items").append(element);
         });
     });
 }
 
-function getBuiltItem(project)
+class Project
 {
-    let date = "";
-    if (project.started && project.ended) 
+    started = null;
+    ended = null;
+    category = null;
+    done = false;
+    content = null;
+
+    constructor(obj) 
     {
-        if (project.started == project.ended)
-        {
-            date = project.started;
-        }
-        else
-        {
-            date = project.started + " bis " + project.ended;
-        }
+        this.started = obj.started;
+        this.ended = obj.ended;
+        this.category = obj.category;
+        this.done = obj.done;
+        this.content = new Content(obj.content);
     }
+    
+    isLinked = () => this.content.link ? true : false;
 
-    let item = getTemplateItem(project.content.link);
-    if (project.content.link) item.attr("href", project.content.link);
-    item.children('.item__date').text(date);
-    item.children('.item__title').text(project.content.title);
-    item.children('.item__subtitle').text(project.content.susbtitle);
+    getElement = () => {
 
-    return item;
+        let date = "";
+        if (this.started && this.ended) 
+        {
+            if (this.started == this.ended)
+            {
+                date = this.started;
+            }
+            else
+            {
+                date = this.started + " bis " + this.ended;
+            }
+        }
+        else{
+            if (this.finished)
+            {
+                date = this.started;
+            }
+            else
+            {
+                date = `seit ${this.started}`;
+            }
+        }
+        
+        let item = $(this.isLinked() ? `<a href="${this.content.link}" target="_blank" class="item item--list"></a>` : `<div class="item item--list">`);
+
+        let left = $(`<div class="item__left">
+            <h3 class="item__title">${this.content.title}</h3>
+            <p class="item__subtitle">${this.content.susbtitle}</p>
+        </div>`);
+        
+        let right = $(`<div class="item__right">
+            <p class="item__date">${date}</p>
+        </div>`);
+
+        item.append(left);
+        item.append(right);
+
+        return item;
+    }
 }
 
-function getTemplateItem(linked)
+class Content
 {
-    let itemTag = (linked != false) ? '<a href="" "target="_blank" class="item item--list"></a>' : '<div class="item item--list">';
-    let item = $(itemTag);
+    title = null;
+    susbtitle = null;
+    link = null;
 
-    item.append('<p class="item__date"></p>');
-    item.append('<h3 class="item__title"></h3>');
-    item.append('<p class="item__subtitle"></p>');
-
-    return item;
+    constructor(obj) 
+    {
+        this.title = obj.title;
+        this.susbtitle = obj.susbtitle;
+        this.link = obj.link;
+    }
 }
